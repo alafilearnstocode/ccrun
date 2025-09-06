d5565f678f1f8751ddf5a6f7ad55e724316ceb37
alafilearnstocode <emmanuelalafi2004@gmail.com>
2025-09-06 16:27:44 -0500

    hostname

--- internal/ns/ns.go
+++ internal/ns/ns.go
@@ -18,6 +18,9 @@ type Config struct {
 	UseUTS   bool
 }
 
+// SpawnChild re-execs the current binary with namespace flags.
+// We pass our child flags (-uts/-hostname) BEFORE the "--",
+// and the target program AFTER the "--".
 func SpawnChild(cfg Config, command string, args []string) (int, error) {
 	self, err := os.Executable()
 	if err != nil {
@@ -26,14 +29,16 @@ func SpawnChild(cfg Config, command string, args []string) (int, error) {
 
 	argv := []string{childSub}
 	if cfg.UseUTS {
-		argv = append(argv, "--uts", "--hostname", cfg.Hostname)
+		argv = append(argv, "-uts", "-hostname", cfg.Hostname)
 	}
-	argv = append(argv, "--", command)
+	argv = append(argv, "--") // separator between child flags and target command
+	argv = append(argv, command)
 	argv = append(argv, args...)
 
 	cmd := exec.Command(self, argv...)
 	cmd.Stdin, cmd.Stdout, cmd.Stderr = os.Stdin, os.Stdout, os.Stderr
 
+	// SysProcAttr defines clone/namespace behavior
 	sp := &syscall.SysProcAttr{}
 	if cfg.UseUTS {
 		sp.Cloneflags |= unix.CLONE_NEWUTS
@@ -49,35 +54,45 @@ func SpawnChild(cfg Config, command string, args []string) (int, error) {
 	return 0, nil
 }
 
+// ChildMain runs inside the namespaced child. It parses child flags (-uts/-hostname),
+// then looks for the "--" separator and executes the target program.
 func ChildMain() {
 	f := flag.NewFlagSet(childSub, flag.ExitOnError)
 	var useUTS bool
 	var hostname string
 	f.BoolVar(&useUTS, "uts", false, "use UTS namespace")
 	f.StringVar(&hostname, "hostname", "", "hostname inside container")
-	f.Parse(os.Args[2:])
-	args := f.Args()
 
+	// Parse child flags from args after the program name and subcommand.
+	// For argv: [ccrun __ccrun_child__ <flags> -- <cmd> <args>...]
+	f.Parse(os.Args[2:])
+	rest := f.Args()
+
+	// Find the `--` that separates flags from the target program.
 	sep := -1
-	for i, a := range args {
+	for i, a := range rest {
 		if a == "--" {
 			sep = i
 			break
 		}
 	}
-	if sep == -1 || sep == len(args)-1 {
+	if sep == -1 || sep == len(rest)-1 {
 		fmt.Fprintln(os.Stderr, "child: missing -- <cmd>")
 		os.Exit(2)
 	}
-	target := args[sep+1]
-	targs := args[sep+2:]
 
+	target := rest[sep+1]
+	targs := rest[sep+2:]
+
+	// Set the hostname if requested
 	if useUTS && hostname != "" {
 		if err := unix.Sethostname([]byte(hostname)); err != nil {
 			fmt.Fprintln(os.Stderr, "sethostname:", err)
 			os.Exit(1)
 		}
 	}
+
+	// Exec the target with inherited stdio and env
 	code, err := run.ExecPassthrough(target, targs, os.Environ())
 	if err != nil && code == 0 {
 		code = 1
